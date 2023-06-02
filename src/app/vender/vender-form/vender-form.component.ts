f06fa42150a2b8112f690fa058560a47d2451b74
JoseBustamanteFerrerue <josbusfer@alu.edu.gva.es>
2023-06-02 23:04:21 +0200

    acabar vender

--- src/app/vender/vender-form/vender-form.component.ts
+++ src/app/vender/vender-form/vender-form.component.ts
@@ -14,10 +14,18 @@ export class VenderFormComponent implements OnInit{
   mostrarForms: number = 1;
   progress = 33;
   type = 'success';
+  valorCoche: any;
+  nameMarkValor: any
+  nameModelValor: any
+  nameVersionValor: any
+  provinciaValor: any
+  anyosDesplegable: any
 
   constructor (private elementRef: ElementRef, private formBuilder: FormBuilder, private venderService: VenderServiceService) {}
 
   ngOnInit() {
+    this.generarNumeros()
+
     this.myForm = this.formBuilder.group({
       matricula: ['', [Validators.required, Validators.pattern('^[A-Z0-9]{4}[A-Z]{3}$')]],
       kilometros: ['', [Validators.required]],
@@ -32,10 +40,48 @@ export class VenderFormComponent implements OnInit{
       fecha_nacimiento: ['', [Validators.required]],
       provincia: ['', [Validators.required]]
     });
+
+    this.myForm3 = this.formBuilder.group({
+      nameMark: ['', [Validators.required]],
+      nameModel: ['', [Validators.required]],
+      nameVersion: ['', [Validators.required]],
+      estadoCoche: ['', [Validators.required]],
+    });
   }
 
   enviarDatosForm() {
-    
+    this.nameMarkValor      = this.myForm3.get('nameMark')!.value
+    this.nameModelValor     = this.myForm3.get('nameModel')!.value
+    this.nameVersionValor   = this.myForm3.get('nameVersion')!.value
+    this.provinciaValor     = this.myForm2.get('provincia')!.value
+
+    this.venderService.marcaId(this.nameMarkValor, this.nameModelValor, this.nameVersionValor).subscribe(
+      idMark => {
+        const marcaId = idMark;
+        this.venderService.provinciaId(this.provinciaValor).subscribe(
+          idProvincia => {
+            const provinciaId = idProvincia;
+
+            const cocheTasado = {
+              idMark: marcaId[0].id,
+              idProvincia: provinciaId[0].id,
+              email: this.myForm2.get('email')!.value,
+              name: this.myForm2.get('name')!.value,
+              primer_apellido: this.myForm2.get('primerApellido')!.value,
+              fecha_nacimiento: this.myForm2.get('fecha_nacimiento')!.value,
+              anyo: this.myForm.get('anyoMatriculacion')!.value,
+              km: this.myForm.get('kilometros')!.value,
+              matricula: this.myForm.get('matricula')!.value,
+              estadoCoche: this.myForm3.get('estadoCoche')!.value
+            }
+            console.log(cocheTasado)
+            this.venderService.guadarCocheTasado(cocheTasado)
+            this.valorCoche = this.venderService.tasarCoche(cocheTasado, marcaId)
+            this.mostrarForms = 0
+          }
+        );
+      }
+    );   
   }
 
   scrollToSection() {
@@ -44,6 +90,7 @@ export class VenderFormComponent implements OnInit{
   }
 
   siguienteForm() {
+    console.log(this.desplegableMarca)
     this.mostrarForms++;
     if (this.mostrarForms === 2) {
       this.progress = 66
@@ -68,6 +115,28 @@ export class VenderFormComponent implements OnInit{
     } 
   }
 
+  cambiarAcontacto () {
+    window.location.href = 'contacto'
+  }
+
+  generarNumeros() {
+    let numeros:any = [];
+    for (let i = 2023; i >= 1999; i--) {
+      if (!numeros.includes(i)) {
+        numeros.push(i);
+      }
+    }
+    this.anyosDesplegable = numeros;
+  }
+
+  cambioMarca () {
+    this.venderService.desplegableModelos(this.myForm3.get('nameMark')!.value)
+  }
+
+  cambioModelo () {
+    this.venderService.desplegableVersion(this.myForm3.get('nameMark')!.value, this.myForm3.get('nameModel')!.value)
+  }
+
   sinSignosValidator(control: AbstractControl): ValidationErrors | null {
     const value = control.value;
     const sinSignos = /^[a-zA-Z0-9\s]*$/;
@@ -90,8 +159,25 @@ export class VenderFormComponent implements OnInit{
   get primerApellido() { return this.myForm2.get('primerApellido'); }
   get fecha_nacimiento() { return this.myForm2.get('fecha_nacimiento'); }
   get provincia() { return this.myForm2.get('provincia'); }
+  // Formulario 3
+  get nameMark() { return this.myForm2.get('nameMark'); }
+  get nameModel() { return this.myForm2.get('nameModel'); }
+  get nameVersion() { return this.myForm2.get('nameVersion'); }
+  get estadoCoche() { return this.myForm2.get('estadoCoche'); }
 
   get desplegableProvincias () {
     return this.venderService.desplegableProvincias
+  }
+
+  get desplegableModelo () {
+    return this.venderService.desplegableModelo
+  }
+
+  get desplegableVersion () {
+    return this.venderService.desplegableVersionCoche
+  }
+
+  get desplegableMarca () {
+    return this.venderService.desplegableMarca
   }
 }
--- src/app/vender/vender.service.service.ts
+++ src/app/vender/vender.service.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VenderServiceService {
   desplegableProvincias:any
+  desplegableModelo: any
+  desplegableMarca: any
+  desplegableVersionCoche: any
+  marcaIdPropiedad: any
+  provinciaIdPropiedad: any
 
   constructor(private http: HttpClient) { 
     this.getDesplegableProvincias()
+    this.desplegableMarcas()
   }
 
   getDesplegableProvincias () {
@@ -17,4 +24,110 @@ export class VenderServiceService {
           this.desplegableProvincias = resp
         })
     }
+
+  desplegableModelos (item:any) {
+    this.http.get<any>('http://localhost/rest/post.php?desplegableModelo&&nameMark='+ item)
+      .subscribe( (resp) => {
+        // Para que no se dupliquen
+        let array:any = []
+        for (const item of resp) {
+          if (!array.includes(item.nameModel)) {
+            array.push(item.nameModel)
+          } 
+        }
+        this.desplegableModelo = array
+      })
+  }
+
+  desplegableMarcas () {
+    this.http.get<any>('http://localhost/rest/post.php?desplegableMarcas')
+      .subscribe( (resp) => {
+        // Para que no se dupliquen
+        let array:any = []
+        for (const item of resp) {
+          if (!array.includes(item.nameMark)) {
+            array.push(item.nameMark)
+          } 
+        }
+        this.desplegableMarca = array
+      })
+  }
+
+  desplegableVersion (nameMark: any, nameModel:any) {
+    this.http.get<any>('http://localhost/rest/post.php?desplegableVersion&&nameMark=' + nameMark + '&&nameModel=' + nameModel)
+      .subscribe( (resp) => {
+        this.desplegableVersionCoche = resp
+        console.log(this.desplegableVersionCoche)
+      })
+  }
+
+  
+  marcaId(nameMark: any, nameModel: any, nameVersion: any): Observable<any> {
+    return this.http.get<any>('http://localhost/rest/post.php?marcaId&&nameMark=' + nameMark + '&&nameModel=' 
+      + nameModel + '&&nameVersion=' + nameVersion)
+  }
+  
+
+  provinciaId (provincia: any): Observable<any> {
+    return this.http.get<any>('http://localhost/rest/post.php?provinciaId&&provincia=' + provincia )
+  }
+
+  guadarCocheTasado (cocheTasado: any) {
+    const cocheTasadoJson = JSON.stringify(cocheTasado)
+    console.log(cocheTasadoJson)
+    this.http.post<any>('http://localhost/rest/post.php/tasar', cocheTasadoJson).subscribe(
+      response => {
+
+      })
+  }
+
+  tasarCoche (cocheTasado: any, marcaId: any): any {
+    const anyos = 2023 - cocheTasado.anyo;
+    const km = cocheTasado.km;
+    const estadoCoche = cocheTasado.estadoCoche
+    let restarValorAnyos = anyos * 250;
+    let restarValorKm;
+    let restarValorEstado = 0;
+    
+    switch (true) {
+      case km > 150000:
+        restarValorKm = 1000;
+        break;
+      case km > 100000:
+        restarValorKm = 700;
+        break;
+      case km > 80000:
+        restarValorKm = 600;
+        break;
+      case km > 40000:
+        restarValorKm = 400;
+        break;
+      case km > 20000:
+        restarValorKm = 300;
+        break;
+      case km > 5000:
+        restarValorKm = 250;
+        break;
+      default:
+        restarValorKm = 100;
+        break;
+    }
+
+    switch (estadoCoche) {
+      case 'Bien':
+        restarValorEstado = 400;
+        break;
+      case 'Regular':
+        restarValorEstado = 600;
+        break;
+      case 'Con desperfectos':
+        restarValorEstado = 1000;
+        break;
+      case 'Mal':
+        restarValorEstado = 1500;
+        break;
+    }
+
+    return marcaId[0].valor - restarValorAnyos - restarValorEstado - restarValorKm
+  }
 }
